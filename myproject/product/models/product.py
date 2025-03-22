d3181fbae1bc05d8a503c9236316d68bce239170
sangam2ishra <sangamkr.mishra@gmail.com>
2025-03-22 11:05:04 +0530

    used signals for deletion

--- myproject/product/models/product.py
+++ myproject/product/models/product.py
@@ -1,11 +1,11 @@
-from mongoengine import *
+from mongoengine import Document, StringField, FloatField, IntField, DateTimeField, ReferenceField
 from django.utils import timezone
-from product.models.product_category import ProductCategory
 
 class Product(Document):
     name = StringField(max_length=200)
     description = StringField()
-    category = ReferenceField(ProductCategory)
+    # Use a string reference to avoid circular import:
+    category = ReferenceField('ProductCategory')  
     price = FloatField(required=True)
     brand = StringField()
     quantity = IntField()
@@ -15,8 +15,7 @@ class Product(Document):
     meta = {'collection': 'products'}
 
     def save(self, *args, **kwargs):
-        self.updated_at=timezone.localtime(timezone.now())
-        print(timezone.localtime(timezone.now()))
+        self.updated_at = timezone.localtime(timezone.now())
         return super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
