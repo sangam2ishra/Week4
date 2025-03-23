60fb4891763d6e84b1c9a3fb4fee6d9be1dbbc5d
sangam2ishra <sangamkr.mishra@gmail.com>
2025-03-23 17:45:10 +0530

    created migrate_product_category

---
+++ myproject/product/management/commands/migrate_product_category.py
@@ -0,0 +1,25 @@
+#to run it, python manage.py migrate_products_category
+from django.core.management.base import BaseCommand
+from product.models.product import Product
+from product.models.product_category import ProductCategory
+
+class Command(BaseCommand):
+    help = 'Migrate existing products by assigning a default category for products without one'
+
+    def handle(self, *args, **options):
+        default_category = ProductCategory.objects(title="Uncategorized").first()
+        if not default_category:
+            default_category = ProductCategory(
+                title="Uncategorized",
+                description="Products without a specific category"
+            )
+            default_category.save()        
+
+        products_without_category = Product.objects(category__exists=False)
+        count=0
+        for product in products_without_category:
+            product.category = default_category
+            product.save()
+            count += 1
+
+        self.stdout.write(self.style.SUCCESS(f"Migrated {count} products to 'Uncategorized' category."))
